bc086fd6566924f98515928643bc8825818ddb61
aliraza29121212 <95703064+aliraza29121212@users.noreply.github.com>
2022-06-22 01:41:22 +0500

    add

---
+++ main.js
@@ -0,0 +1,9 @@
+// let ChangeImage = () => {
+//     try {
+//       let SelectedObject = document.querySelector(".FirstImage");
+//       SelectedObject.src = "Images/Mask-2.svg";
+     
+//     } catch (error) {
+//       error.message;
+//     }
+//   };
